f3c0898f8bc26df5398caf60048951ca821be9ed
Mahmoud Osman <mahmoudosman.som@gmail.com>
2022-09-23 22:28:52 -0500

    updated ProductList.tsx component

--- frontend/src/components/products/ProductList.tsx
+++ frontend/src/components/products/ProductList.tsx
@@ -24,20 +24,34 @@ const ProductList = () => {
 	return (
 		<section className="product-list">
 			<div className="container">
-				<h1 className="text-success">Product List</h1> <hr />
+				<p className="text-center">
+					<small>New</small>
+					<br /> <b className="fs-1">iPhone Series</b> <br />
+					<b className="fw-bold fs-6 mb-3">Pro. Beyond.</b>
+				</p>
+				{/* <h1 className="text-success">Product List</h1> <hr /> */}
 				<div className="row">
 					{products.map((product) => (
-						<div className="col-md-3" key={product["id"]}>
-							<div className="card" style={{ minHeight: "30rem" }}>
-								<img
-									src="https://source.unsplash.com/1600x900/?iphone"
-									className="card-img-top"
-									alt={product["name"]}
-								/>
+						<div className="col-md-3 " key={product["id"]}>
+							<div
+								className="card mb-4 shadow shadow-lg rounded "
+								style={{ minHeight: "30rem" }}
+							>
+								<Link to={"/products/" + product["id"]}>
+									<img
+										src="https://source.unsplash.com/1600x900/?iphone"
+										className="card-img-top"
+										alt={product["name"]}
+									/>
+								</Link>
+
 								<div className="card-body">
 									<h3 className="card-title fw-bold text-secondary">
 										{product["name"]}
 									</h3>
+									<p className="btn btn-outline-dark  fw-bold rounded-pill">
+										<b>${product["price"]}</b>
+									</p>
 									<p className="card-text text-muted">
 										{product["description"]}
 									</p>
