89e6b83e8e73a91eb52ec74b55a90e70631e6865
Mahmoud Osman <mahmoudosman.som@gmail.com>
2022-10-19 19:55:18 -0500

    added swagger UI

--- backend/src/main/java/com/northern/controllers/ProductController.java
+++ backend/src/main/java/com/northern/controllers/ProductController.java
@@ -1,21 +1,16 @@
 package com.northern.controllers;
 
-
 import com.northern.models.Product;
 import com.northern.services.ProductService;
 import com.northern.utils.custom_exceptions.InvalidUserException;
-import com.northern.utils.custom_exceptions.NotFoundException;
-import lombok.var;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.logging.Logger;
 
 
 @CrossOrigin(origins = "http://localhost:3000")
@@ -23,6 +18,7 @@ import java.util.Map;
 @RequestMapping("/api/v1")
 public class ProductController {
     private final ProductService productService;
+    Logger log = Logger.getGlobal();
 
     public ProductController(ProductService productService) {
         this.productService = productService;
@@ -39,6 +35,7 @@ public class ProductController {
     //@Get
     @GetMapping(value = "/products/list", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<Product>> getAllProducts() throws IOException {
+
         List<Product> products = productService.getAll();
         return new ResponseEntity<List<Product>>(products, HttpStatus.OK);
     }
@@ -46,6 +43,7 @@ public class ProductController {
     //@Post
     @PostMapping(value = "/products/create", consumes = "application/json", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Product> createProduct(@RequestBody Product product) throws IOException {
+        log.info("Create new product!!");
         Product createdProduct = productService.save(product);
         return new ResponseEntity<Product>(createdProduct, HttpStatus.CREATED);
     }
@@ -78,6 +76,7 @@ public class ProductController {
     //@Delete
     @DeleteMapping(value = "/products/delete/{id}")
     public ResponseEntity<?> deleteProduct(@PathVariable Long id) {
+        log.info("Delete product with id: " + id );
         try {
             productService.delete(id);
             return new ResponseEntity<>("Product has been deleted successfully!!", HttpStatus.ACCEPTED);
